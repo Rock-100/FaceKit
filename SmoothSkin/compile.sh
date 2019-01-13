cbf5d6aa52bc6caff6dcff3c659947b3f7672845
Jack-CV <38135837+Jack-CV@users.noreply.github.com>
2019-01-13 21:45:30 +0800

    add SmoothSkin
    add SmoothSkin

--- SmoothSkin/compile.sh
+++ SmoothSkin/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/en sh
-echo "compile Beauty"
+echo "compile picture"
 g++ -o picture picture.cpp Common.h Common.cpp GuideFilter.h GuideFilter.cpp BilateralFilter.h BilateralFilter.cpp -std=c++11 -O3 `pkg-config --cflags --libs opencv`
 echo "done"
